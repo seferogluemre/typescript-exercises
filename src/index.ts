0d351e0ef6b85f22e0c8f8279c2c8178a5803762
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-14 20:49:58 +0300

    array object params

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,32 @@ function greet(name: string): string {
 const user = "World";
 console.log(greet(user));
 
-let id: string | number = 23;
+let id: string | number | boolean = 23;
 id = "Emre";
+id = true;
+
+let numbers: number[] = [1, 2, 3, 4, 5];
+let strings: Array<string> = ["Ahmet", "Emre", "Berat"];
+let mixedArray: (string | number)[] = ["Emre", 16];
+
+mixedArray.push("Emre");
+
+let anyType: any = true;
+
+interface UserParams {
+  name: string;
+  age: number;
+  isEmployed: boolean;
+}
+
+type User = {
+  name: string;
+  age: number;
+  isEmployed: boolean;
+};
+
+let person: UserParams = {
+  name: "Emre",
+  age: 16,
+  isEmployed: true,
+};

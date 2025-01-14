4932d608371b864db3285b94c07d3cd2b576122b
xEmre1-53 <157003672+xEmre1-53@users.noreply.github.com>
2025-01-14 20:29:25 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("TYPESCRİPT ÇALIŞIYOR");
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const user = "World";
+console.log(greet(user));
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+console.log("TYPESCRİPT ÇALIŞIYOR");
+
+function greet(name: string): string {
+  return `Hello, ${name}!`;
+}
+
+const user = "World";
+console.log(greet(user));
